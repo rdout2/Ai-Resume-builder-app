15f707fb7eeb81a0c851a9ea924bd648bcf0cf7e
ahmat1 <rahmatmahamatdout@gmail.com>
2025-01-15 14:06:35 +0100

    Confique authentificTION oK

---
+++ src/auth/sing-in/index.jsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/clerk-react'
+import React from 'react'
+
+function SingInPage() {
+  return (
+    <div>
+      <SignIn/>
+    </div>
+  ) 
+}
+
+export default SingInPage
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SingInPage from "./auth/sing-in/index.jsx";
+import Dashboard from "./dashboard/index.jsx";
+import Home from "./home/index.jsx"; 
+import { ClerkProvider } from '@clerk/clerk-react'
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/", 
+        element: <Home />, 
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/auth/sign-in",
+    element: <SingInPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+);
+
